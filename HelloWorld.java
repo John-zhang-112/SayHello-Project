c02cac8f933d97fd8b81be5f6f08bca757d6d95c
Zhang <zh1061008729@qq.com>
2019-06-11 11:49:01 +0800

    working program HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+     public	static void main(String[] args){
+		 System.out.println("Hello,world!");
+		 sayMorning();
+	 }
+	 public static void sayMorning(){
+		 int i=0,sum=0;
+		 while(i<-100){
+			 sum=+i;
+			 i++;
+		 }
+		 System.out.println("sum:"+sum);
+	 }
+}
